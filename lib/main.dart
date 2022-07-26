1f93e4c4b5f618161e1d2eebc204504d9dda2cbf
takaku1222 <tomo15161222@icloud.com>
2022-07-26 11:02:32 +0900

    firebase_coreとfirebase_analyticsをpubspec.yamlに追加 flutter run --dart-define=FLAVOR=devとflutter run --dart-define=FLAVOR=prodのbuildは成功

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 
-void main() async {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
