71b3bb32303ff64c2ecf48b7e042c9aa924b23c1
Kenta Wakasa <wakasa.kenta.pg@gmail.com>
2022-08-06 06:59:51 +0900

    すでにログイン中であればサインインページを表示

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:joyato/home_page.dart';
 import 'package:joyato/sign_in_page.dart';
 
 Future<void> main() async {
@@ -13,12 +15,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const SignInPage(),
+    final currentUser = FirebaseAuth.instance.currentUser;
+
+    // currentUserがnullならログインしていない
+    // その場合はSingInPageを表示する
+    if (currentUser == null) {
+      return const MaterialApp(
+        home: SignInPage(),
+      );
+    }
+
+    return const MaterialApp(
+      home: HomePage(),
     );
   }
 }
