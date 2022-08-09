9befe10eca96f8fdc37f7dc7c249386079fc6f68
“samuraituru” <samuraituru.dev@gmail.com>
2022-08-09 23:04:42 +0900

    google_maps_flutterを最小構成で実装、API-KEY設定

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:joyato/SignIn_page.dart';
 
@@ -11,6 +14,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _controller = Completer<GoogleMapController>();
+  final _markers = <Marker>{};
+  final initLatLng = const LatLng(35.675, 139.770);
+  final double maxZoomLevel = 18;
+  final double minZoomLevel = 6;
+  late final _initPosition = CameraPosition(target: initLatLng, zoom: 14.0);
+
   Future<void> singOut() async {
     await GoogleSignIn().signOut();
     await FirebaseAuth.instance.signOut();
@@ -21,31 +31,45 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('ホームページ'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            showDialog(
-              context: context,
-              builder: (context) {
-                return const Center(
-                  child: CircularProgressIndicator(),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.exit_to_app),
+            onPressed: () {
+              onPressed:
+              () async {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  },
                 );
-              },
-            );
-            await singOut();
-            if (!mounted) {
-              return;
-            }
-            Navigator.of(context).pushAndRemoveUntil(
-              MaterialPageRoute(builder: (context) {
-                return const SignInPage();
-              }),
-              (route) => false,
-            );
-          },
-          child: const Text('Googleサインアウト'),
-        ),
+                await singOut();
+                if (!mounted) {
+                  return;
+                }
+                Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(builder: (context) {
+                    return const SignInPage();
+                  }),
+                  (route) => false,
+                );
+              };
+            },
+          )
+        ],
+      ),
+      body: GoogleMap(
+        mapType: MapType.terrain,
+        initialCameraPosition: _initPosition,
+        markers: _markers,
+        minMaxZoomPreference: MinMaxZoomPreference(minZoomLevel, maxZoomLevel),
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
