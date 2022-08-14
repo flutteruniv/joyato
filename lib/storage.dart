c916f33fec23868ced3527516dbc45fddbe0e09e
“samuraituru” <samuraituru.dev@gmail.com>
2022-08-14 11:49:23 +0900

    各種ファイルの整理

--- lib/storage.dart
+++ lib/storage.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-import 'pages/account.dart';
+import 'domain/account.dart';
 
 const String accounts = 'accounts';
 
