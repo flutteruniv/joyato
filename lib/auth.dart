5603084e2beadb17ed7101d7cab7f52798b89f11
Kenta Wakasa <wakasa.kenta.pg@gmail.com>
2022-08-14 15:19:27 +0900

    こんぶレビュー

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,19 +1,44 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-Future<UserCredential> signInWithGoogle() async {
-  final googleUser = await GoogleSignIn().signIn();
-  final googleAuth = await googleUser?.authentication;
-  final credential = GoogleAuthProvider.credential(
-    accessToken: googleAuth?.accessToken,
-    idToken: googleAuth?.idToken,
-  );
-  final userCredential =
-      await FirebaseAuth.instance.signInWithCredential(credential);
-  return userCredential;
+final firebaseAuthProvider =
+    Provider<FirebaseAuth>((ref) => FirebaseAuth.instance);
+
+final googleSignInProvider = Provider<GoogleSignIn>(
+  (ref) => GoogleSignIn(),
+);
+
+final authRepositoryProvider = Provider((ref) {
+  return AuthRepository(ref.read);
+});
+
+class AuthRepository {
+  AuthRepository(this._read);
+  final Reader _read;
+
+  /// プロバイドされたFirebaseAuthのインスタンス
+  late final firebaseAuth = _read(firebaseAuthProvider);
+
+  late final googleSignIn = _read(googleSignInProvider);
+
+  Future<UserCredential> signInWithGoogle() async {
+    final googleUser = await googleSignIn.signIn();
+    final googleAuth = await googleUser?.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+    final userCredential = await firebaseAuth.signInWithCredential(credential);
+    return userCredential;
+  }
+
+  Future<void> singOut() async {
+    await googleSignIn.signOut();
+    await firebaseAuth.signOut();
+  }
 }
 
-Future<void> singOut() async {
-  await GoogleSignIn().signOut();
-  await FirebaseAuth.instance.signOut();
-}
+final authStateChangesProvider = StreamProvider<User?>((ref) {
+  return ref.read(firebaseAuthProvider).authStateChanges();
+});
