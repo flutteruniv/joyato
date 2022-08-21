90f6a9c44c5df813ab375522f01f9895b22a73b6
“samuraituru” <samuraituru.dev@gmail.com>
2022-08-21 10:34:14 +0900

    マーカー作成メソッドの定義

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -23,7 +25,7 @@ class _HomePageState extends ConsumerState<HomePage> {
     _maxZoomLevel,
   );
 
-  /// 初期配置
+  /// 地図初期配置
   static const _initLatLng = LatLng(35.675, 139.770);
   static const _initPosition = CameraPosition(target: _initLatLng, zoom: 14.0);
 
@@ -49,7 +51,7 @@ class _HomePageState extends ConsumerState<HomePage> {
     if (!mounted) {
       return;
     }
-   navigator.pop();
+    navigator.pop();
   }
 
   @override
@@ -74,7 +76,63 @@ class _HomePageState extends ConsumerState<HomePage> {
         onMapCreated: (controller) {
           _controller.complete(controller);
         },
+
+        /// onTap時に [canvasMarkerCreate] を実行する
+        onTap: (latLng) async {
+          await canvasMarkerCreate(latLng);
+        },
       ),
     );
   }
+
+  /// CanvasMarkerを作成する関数
+  Future<void> canvasMarkerCreate(LatLng latLng) async {
+    /// 初期Canvasサイズ
+    final markerIcon = await getBytesFromCanvas(50, 50);
+
+    final initMarker = Marker(
+      markerId: MarkerId(latLng.toString()),
+      icon: BitmapDescriptor.fromBytes(markerIcon),
+      position: latLng,
+      //onTap: () => onTapCanvasCallBack(latLng),
+    );
+
+    setState(() {
+      _markers.add(initMarker);
+    });
+  }
+}
+
+/// 引数からUint8List型でCancvasをリターンする関数
+Future<Uint8List> getBytesFromCanvas(int width, int height) async {
+  final pictureRecorder = ui.PictureRecorder();
+  final canvas = Canvas(pictureRecorder);
+  final paint = Paint()..color = Colors.red;
+  const radius = Radius.circular(30.0);
+  const text = 'P';
+  const textStyle = TextStyle(fontSize: 25.0, color: Colors.white);
+
+  canvas.drawRRect(
+      RRect.fromRectAndCorners(
+        Rect.fromLTWH(0.0, 0.0, width.toDouble(), height.toDouble()),
+        topLeft: radius,
+        topRight: radius,
+        bottomLeft: radius,
+        bottomRight: radius,
+      ),
+      paint);
+
+  final painter = TextPainter(textDirection: TextDirection.ltr);
+  painter.text = const TextSpan(
+    text: text,
+    style: textStyle,
+  );
+  painter.layout();
+  painter.paint(
+      canvas,
+      Offset((width * 0.5) - painter.width * 0.5,
+          (height * 0.5) - painter.height * 0.5));
+  final img = await pictureRecorder.endRecording().toImage(width, height);
+  final data = await img.toByteData(format: ui.ImageByteFormat.png);
+  return data!.buffer.asUint8List();
 }
