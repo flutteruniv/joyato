d05ce626332a0b627041fabb10ea5ac5f602f420
“samuraituru” <samuraituru.dev@gmail.com>
2022-09-09 22:07:50 +0900

    コメントの修正

--- lib/pages/post_page.dart
+++ lib/pages/post_page.dart
@@ -21,50 +21,52 @@ class PostPage extends ConsumerWidget {
     return Stack(
       children: [
         WillPopScope(
-        onWillPop: () {Navigator.of(context).pop(false);
-    return Future.value(false);},
-        child:
-        Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            title: const Text('投稿'),
-            //leading: const Icon(Icons.arrow_back),
-          ),
-          body: Center(
-            child: Column(
-              children: [
-                const Text('タイトル'),
-                TextFormField(
-                  controller: titleControllerProvider.state,
-                ),
-                const Text('内容'),
-                TextFormField(
-                  controller: bodyControllerProvider.state,
-                ),
-                ElevatedButton(
-                    onPressed: () async {
-                      // ToDO. ピン情報を更新しなかった場合の動作を実装する
-                      try {
-                        isLoading = true;
-                        await postRepository
-                            .storePinToPostCorrection(geoFirePoint);
-                        //postRepository.initializeTextEditingController();
-                        navigator.pop();
-                      } catch (e) {
-                        final snackBar = SnackBar(
-                          backgroundColor: Colors.red,
-                          content: Text(e.toString()),
-                        );
-                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                      } finally {
-                        isLoading = false;
-                      }
-                    },
-                    child: const Text('投稿する'))
-              ],
+          // 投稿画面Pop時に前の画面にTrueを返す
+          onWillPop: () {
+            navigator.pop(true);
+            return Future.value(false);
+          },
+          child: Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              title: const Text('投稿'),
+            ),
+            body: Center(
+              child: Column(
+                children: [
+                  const Text('タイトル'),
+                  TextFormField(
+                    controller: titleControllerProvider.state,
+                  ),
+                  const Text('内容'),
+                  TextFormField(
+                    controller: bodyControllerProvider.state,
+                  ),
+                  ElevatedButton(
+                      onPressed: () async {
+                        try {
+                          isLoading = true;
+                          await postRepository
+                              .storePinToPostCorrection(geoFirePoint);
+                          // 投稿が行われた場合、前の画面にTrueを返す
+                          navigator.pop(true);
+                        } catch (e) {
+                          final snackBar = SnackBar(
+                            backgroundColor: Colors.red,
+                            content: Text(e.toString()),
+                          );
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                          // エクセプションが行われた場合前の画面にFalseを返す
+                          navigator.pop(false);
+                        } finally {
+                          isLoading = false;
+                        }
+                      },
+                      child: const Text('投稿する'))
+                ],
+              ),
             ),
           ),
-        ),
         ),
         if (isLoading)
           const ColoredBox(
@@ -75,29 +77,5 @@ class PostPage extends ConsumerWidget {
           ),
       ],
     );
-  }
-
-  Future<bool> _backButtonPress(BuildContext context) async {
-    final answer = await showDialog<bool>(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text('Dialog'),
-            actions: [
-              TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop(false);
-                  },
-                  child: const Text('No')),
-              TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop(true);
-                  },
-                  child: const Text('Yes'))
-            ],
-          );
-        });
-
-    return answer ?? false;
   }
 }
